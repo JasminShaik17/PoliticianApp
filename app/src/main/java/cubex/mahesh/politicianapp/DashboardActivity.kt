d18e0ca1e67fe5c3aa449c7d5b307100b0f3a592
maheshthippala <mahesh.cubex@gmail.com>
2018-06-01 23:09:38 +0530

    added reegistration screen

--- app/src/main/java/cubex/mahesh/politicianapp/DashboardActivity.kt
+++ app/src/main/java/cubex/mahesh/politicianapp/DashboardActivity.kt
@@ -2,11 +2,26 @@ package cubex.mahesh.politicianapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+    }
+
+    fun  load(v:View)
+    {
+        when(v.id)
+        {
+            R.id.previous_events-> print("")
+            R.id.next_events-> print("")
+            R.id.messages-> print("")
+            R.id.complaint-> print("")
+            R.id.user-> print("")
+            R.id.contactus-> print("")
+        }
+
     }
 }
